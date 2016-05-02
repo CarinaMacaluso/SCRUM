365d3c3282f0c8697f992cf065bceb5f8d69132c
Carina M <carina.macaluso@gmail.com>
2016-05-02 23:24:39 +0200

    Player walk and jump

--- SCRUM GAME 2016/Assets/PlayerWalk.cs
+++ SCRUM GAME 2016/Assets/PlayerWalk.cs
@@ -1,15 +1,32 @@
-﻿using UnityEngine;
+﻿ausing UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
-public class PlayerWalk : MonoBehaviour {
+public class PlayerWalk : MonoBehaviour
+{
+
+	public float moveSpeed;
+	public float rotationSpeed;
+	public float jumpStrength;
+	Rigidbody rb;
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody> ();
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
-	
+	void Update ()
+	{
+		float h = Input.GetAxis ("Horizontal");		//key 'A' or 'D' / Arrow left/right
+		float v = Input.GetAxis ("Vertical"); 		//key 'W' or 'S' / Arrow up/down
+		transform.Translate (new Vector3 (0, 0, v) * Time.deltaTime * moveSpeed);
+		transform.Rotate (0, h * rotationSpeed, 0);
+
+		if (Input.GetKeyDown (KeyCode.Space)) { 
+			rb.AddForce (Vector3.up * 800F * jumpStrength);
+		}
 	}
+
 }
