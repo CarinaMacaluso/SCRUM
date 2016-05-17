cb30531e6c381d47251f0cc459adc0ee5a3d7534
Carina M <carina.macaluso@gmail.com>
2016-05-17 18:49:54 +0200

    Laufbahn- & Anschiebemodus

--- SCRUM GAME 2016/Assets/PlayerWalk.cs
+++ SCRUM GAME 2016/Assets/PlayerWalk.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿
+
+using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
@@ -10,11 +12,15 @@ public class PlayerWalk : MonoBehaviour
 	public float rotationSpeed;
 	public float jumpStrength;
 	public int score;
-	public Text scoreText; 
+	public Text scoreText;
 	public int health;
 	public bool isShieldActive;
 	public int shieldCounter;
+	public GameObject centerLine;
+	public GameObject leftLine;
+	public GameObject rightLine;
 	Rigidbody rb;
+	public int currentLine;
 
 	// Use this for initialization
 	void Start ()
@@ -24,37 +30,58 @@ public class PlayerWalk : MonoBehaviour
 		health = 100; 
 		isShieldActive = false; 
 		shieldCounter = 0; 
+		currentLine = 0; 
 
 	}
 
 	// Update is called once per frame
+
 	void Update ()
 	{
-		float h = Input.GetAxis ("Horizontal");		//key 'A' or 'D' / Arrow left/right
-		float v = Input.GetAxis ("Vertical"); 		//key 'W' or 'S' / Arrow up/down
-		transform.Translate (new Vector3 (0, 0, v) * Time.deltaTime * moveSpeed);
-		transform.Rotate (0, h * rotationSpeed, 0);
+
+		transform.Translate (new Vector3 (0, 0, 1) * moveSpeed);
+
+		if (Input.GetKeyDown ("left")) { // left
+			print ("Left");
+			if (currentLine == 0) {
+				transform.position = new Vector3 (leftLine.transform.position.x, transform.position.y, transform.position.z);
+				currentLine = 1; 
+			} else if (currentLine == -1) {
+				transform.position = new Vector3 (centerLine.transform.position.x, transform.position.y, transform.position.z);
+				currentLine = 0; 
+			}
+
+		} else if (Input.GetKeyDown ("right")) { //right
+			if (currentLine == 0) {
+				transform.position = new Vector3 (rightLine.transform.position.x, transform.position.y, transform.position.z);
+				currentLine = -1;
+			} else if (currentLine == 1) {
+				transform.position = new Vector3 (centerLine.transform.position.x, transform.position.y, transform.position.z);
+				currentLine = 0;
+			}
+		}
 
 		if (Input.GetKeyDown (KeyCode.Space)) { 
 			rb.AddForce (Vector3.up * 800F * jumpStrength);
 		}
 
-			if (isShieldActive == true) {
-				shieldCounter++;
-				if (shieldCounter == 1000) {
-					isShieldActive = false;
-					shieldCounter = 0;
+		if (isShieldActive == true) {
+			shieldCounter++;
+			if (shieldCounter == 1000) {
+				isShieldActive = false;
+				shieldCounter = 0;
 				print ("Item vorbei"); 
-				} 
+			} 
 		}
 	}
 
-	void OnCollisionEnter(Collision coll) {
+	void OnCollisionEnter (Collision coll)
+	{
 		if (coll.gameObject.tag == "Coin") {
 			Destroy (coll.gameObject);
 			score++;
 			print (score);
-			scoreText.text = "Coin "+score; 
+			scoreText.text = "Coin " + score; 
 		} 
 
 		if (coll.gameObject.tag == "HealthItem") {
