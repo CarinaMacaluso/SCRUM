34cf739395a0d5d2e46f0eda1808bad80d28f2be
Anna-Lena Hösl <annalenahoesl@gmx.de>
2016-06-04 23:58:53 +0200

    Pausenmenü unfertiges script

---
+++ SCRUM GAME 2016/Assets/pauseGame.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pauseGame : MonoBehaviour {
+	public Transform pauseCanvas;
+	public CameraScript script;
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.P))
+	
+			{
+			if (pauseCanvas.gameObject.activeInHierarchy == false) {
+				pauseCanvas.gameObject.SetActive (true);
+
+			} else {
+				pauseCanvas.gameObject.SetActive (false);
+
+			}
+			}
+	}
+}
