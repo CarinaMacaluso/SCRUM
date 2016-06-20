30e2e60f2791d76ab28d8b6d940d2adcee7d61d0
Carina M <carina.macaluso@gmail.com>
2016-06-20 21:35:52 +0200

    Skript zum random-Hinzufügen von Hindernissen fertig.
    Funktionalität ist derzeit noch deaktiviert.

--- SCRUM GAME 2016/Assets/RandomGameObjects.cs
+++ SCRUM GAME 2016/Assets/RandomGameObjects.cs
@@ -1,12 +1,42 @@
 ﻿using System;
+using UnityEngine;
 
-namespace AssemblyCSharp
+public class RandomGameObjects : MonoBehaviour
 {
-	public class RandomGameObjects
+	public GameObject parent;
+
+	public GameObject leftLane;
+	public GameObject centerLane;
+	public GameObject rightLine;
+
+
+	public GameObject rocks1;
+	public GameObject rocks2;
+	public GameObject rocks3;
+
+	private GameObject[] lanes;
+	private GameObject[] rocks;
+	public int DENSITY;
+
+	float length;
+
+	void Start ()
 	{
-		public RandomGameObjects ()
-		{
+		lanes = new GameObject[] {leftLane, centerLane, rightLine};
+		rocks = new GameObject[] {rocks1,rocks2,rocks3};
+		length = centerLane.transform.lossyScale.z*10;
+		print ("length: "+ length);
+
+		for (int i = 0; i < DENSITY; i++) {
+			int randomLane = (int)UnityEngine.Random.Range (0, lanes.Length);
+			int randomObject = (int)UnityEngine.Random.Range (0, rocks.Length);
+			float posX = lanes [randomLane].transform.position.x;
+			float posZ = UnityEngine.Random.Range (100,length-200);
+
+			GameObject instace = (GameObject) Instantiate (rocks [randomObject], new Vector3 (posX, 0, posZ),Quaternion.identity);
+			instace.transform.parent = parent.gameObject.transform;
 		}
 	}
+
 }
 
