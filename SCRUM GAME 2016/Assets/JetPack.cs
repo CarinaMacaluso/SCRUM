d9a98978c4dce5557d0283d09ec26f73e22d1dce
pxtricix <patricia_frischko@arcor.de>
2016-06-25 14:43:37 +0200

    JetPack GUI
    -JetPack GUI gescriptet
    -Beim Einsammeln vom Tank wird TankGUI sichtbar
    -Beim drücken von "J" (PowerJump) verschwindet sie wieder

---
+++ SCRUM GAME 2016/Assets/JetPack.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class JetPack : MonoBehaviour {
+
+	public Image jetpack;
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+		jetpack.fillAmount = 0; 
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.J)) {
+			jetpack.fillAmount = 0;
+		}
+	}
+
+	void OnTriggerEnter (Collider other) {
+		if (other.tag == "JetPack") {
+			jetpack.fillAmount = 1;
+		} 
+		
+	}
+}
