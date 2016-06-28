f8f9c83f13f5accb2f27df61c27f457023c3222f
annalena01 <annalenahoesl@gmx.de>
2016-06-28 15:01:20 +0200

    Hindernisse fertig; Bugfixes
    Shield -> FAIL

---
+++ SCRUM GAME 2016/Assets/TriggerfuerBaum.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerfuerBaum : MonoBehaviour {
+
+	public Animation anim;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter (Collider other) {
+		if (other.tag == "TriggerBaum") {
+			anim.Play ();
+
+		}
+	}
+}
