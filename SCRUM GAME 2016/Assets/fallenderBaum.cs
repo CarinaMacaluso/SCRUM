25e9ff2db7c9f7a1a8ac246cbb364e6b867f8922
annalena01 <annalenahoesl@gmx.de>
2016-07-05 14:17:12 +0200

    script-versuch
    Noch nicht fertig!!!!

---
+++ SCRUM GAME 2016/Assets/fallenderBaum.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fallenderBaum : MonoBehaviour
+{
+	public bool fB1;
+	private GameObject player;
+	private Animator anim;
+
+
+
+
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+		
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+
+	void OnTriggerEnter (Collider other)
+	{
+
+			
+	}
+}
+
