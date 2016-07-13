2ba956a010c98b65c7ed7d142970716b0768dd45
Anna-Lena Hösl <annalenahoesl@gmx.de>
2016-07-13 09:21:21 +0200

    musik patricia hinzugefügt

--- SCRUM GAME 2016/Assets/Timer.cs
+++ SCRUM GAME 2016/Assets/Timer.cs
@@ -8,45 +8,43 @@
 //
 //public class Timer : MonoBehaviour {
 //
-//    public float time = 5;
+ //   public float time = 5;
 //    public float remainingTime;
 //    public bool timerSet = false;
 //    private GameObject TimerText;
 //    private Text text;
-//
-//
 //
 //    void Start()
 //    {
 //        TimerText = GameObject.FindWithTag("TimerText");
 //        text = TimerText.GetComponent<Text>();
 //
-//    }
+ //  }
 //
 //
-//    // Use this for initialization
+    // Use this for initialization
 //    void OnTriggerEnter (Collider other) {
 //	    if(other.gameObject.tag == "Player")
-//        {
-//            remainingTime = time;
+  //      {
+ //           remainingTime = time;
 //            timerSet = true;
 //		
 //
-//        }
+//}
 //	}
-//	
-//	// Update is called once per frame
+//
+	// Update is called once per frame
 //	void Update () {
 //	    if(timerSet == true)
-//        {
-//            text.text = remainingTime.ToString("F2");
+//{
+ //           text.text = remainingTime.ToString("F2");
 //            remainingTime -= Time.deltaTime;
 //        }
 //
 //        if(remainingTime <= 0)
 //        {
 //            timerSet = false;
-//            text.text = " ";
+ //           text.text = " ";
 //
 //        }
 //	}
