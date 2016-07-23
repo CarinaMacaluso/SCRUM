fe6e6d1d1942063103b16f48acfcc28bc1e100db
Anna-Lena Hösl <annalenahoesl@gmx.de>
2016-07-23 16:58:54 +0200

    münzen schneller gemacht; script für onDistance angefange

--- SCRUM GAME 2016/Assets/CoinRotation.cs
+++ SCRUM GAME 2016/Assets/CoinRotation.cs
@@ -11,7 +11,7 @@ public class CoinRotation : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-		transform.Rotate (Vector3.up, 20 * Time.deltaTime);
+		transform.Rotate (Vector3.up, 60 * Time.deltaTime);
 	
 	}
 }
---
+++ SCRUM GAME 2016/Assets/animDistance.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class animDistance : MonoBehaviour {
+	private AnimationClip clipName;
+	public float reachDistance;
+	public GameObject Player;
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Debug.Log (Vector3.Distance (Player.transform.position, transform.position));
+
+
+	}
+}
