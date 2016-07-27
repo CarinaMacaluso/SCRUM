211f6859090753b8d2fa243bdb486199f9cb54b4
pxtricix <patriciafrischko@Patricias-iMac.local>
2016-07-27 10:31:44 +0200

    Return to Menu Button im Pausenmenü

---
+++ SCRUM GAME 2016/Assets/returnmenu.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class returnmenu : MonoBehaviour {
+
+	public Button returnmenue;
+
+
+	// Use this for initialization
+	void Start () {
+	
+		returnmenue = returnmenue.GetComponent<Button> (); 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void StartLevel () {
+		Application.LoadLevel ("Menue");
+		
+	}
+}
