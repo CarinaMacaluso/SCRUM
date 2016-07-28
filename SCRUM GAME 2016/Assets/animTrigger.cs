998d44296d71d57b051dd087d2a24fe7f2aeaa3d
Anna-Lena Hösl <annalenahoesl@gmx.de>
2016-07-28 14:26:30 +0200

    OnTriggerScript erstellt

---
+++ SCRUM GAME 2016/Assets/animTrigger.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class animTrigger : MonoBehaviour {
+
+	public Animator anim;
+
+
+	// Use this for initialization
+	void Start () {
+		anim.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void OnTriggerEnter (){
+		anim.enabled = true;
+	}
+}
---
+++ SCRUM GAME 2016/Assets/scoreScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class scoreScript : MonoBehaviour {
+
+	public Text score;
+	public Text highScore;
+
+
+	// Use this for initialization
+	void Start () {
+		score.text = "Score: " + PlayerWalk.score;
+		highScore.text = "Highscore: " + PrefsHelper.getHighestScore ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
