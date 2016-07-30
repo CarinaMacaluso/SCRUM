b7f66ad9be36acea86c4ead423ba37f541c9969b
pxtricix <patriciafrischko@Patricias-iMac.local>
2016-07-30 21:18:44 +0200

    sounds verbessert und ufo hui

--- SCRUM GAME 2016/Assets/animTrigger.cs
+++ SCRUM GAME 2016/Assets/animTrigger.cs
@@ -4,15 +4,20 @@ using System.Collections;
 public class animTrigger : MonoBehaviour {
 
 	public Animator anim;
+	private AudioSource audio;
 
 
 	// Use this for initialization
 	void Start () {
 		anim.enabled = false;
+
+		audio = GetComponent<AudioSource> (); 
+		audio.mute = true; 
 	}
 	
 	// Update is called once per frame
 	void OnTriggerEnter (){
 		anim.enabled = true;
+		audio.mute = false; 
 	}
 }
